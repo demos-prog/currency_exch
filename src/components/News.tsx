777f7dd19c45c46bf1ec392f91878a08ea5decd2
Дмитрий <demosne@gmail.com>
2023-10-10 10:47:42 +0300

    gg

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './News.css';
 
-// const api_token = '6523f88c3396f5.01422456';
-// async function get_data() {
-//   return (await fetch(`https://eodhd.com/api/news?api_token=${api_token}&s=AAPL.US&offset=0&limit=10`)).json();
-// }
+const api_token = '6523f88c3396f5.01422456';
+async function get_data() {
+  return (await fetch(`https://eodhd.com/api/news?api_token=${api_token}&s=AAPL.US&offset=0&limit=10`)).json();
+}
 
 type News_type = {
   date: string,
@@ -17,19 +17,12 @@ const News: React.FC = () => {
   const [data, setData] = useState<News_type[]>([]);
 
   useEffect(() => {
-    // get_data().then((data) => {
-    //   setData(data);
-    //   window.localStorage.setItem("data", JSON.stringify(data))
-    // })
-
-    setData(JSON.parse(window.localStorage.getItem('data') || 'null'));
-
+    get_data().then((data) => {
+      setData(data);
+      // window.localStorage.setItem("data", JSON.stringify(data))
+    })
+    // setData(JSON.parse(window.localStorage.getItem('data') || 'null'));
   }, [])
-
-
-
-  console.log(data);
-
 
   return (
     <div id='news_wrap'>
